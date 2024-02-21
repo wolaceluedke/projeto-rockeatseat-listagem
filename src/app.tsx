c86c1a65c3cd5c0f5d692aaca703f8eeefc708dd
Wolace Luedke <wolacepin@hotmail.com>
2024-02-21 17:23:16 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+import { Plus } from 'lucide-react';
+
+
+
+export function App() {
+
+  return (
+     <div className="py-10 space-y-8">
+      <div>
+      header
+      tabs
+     </div>
+     <main className="max-w-6xl mx-auto space-y-5 ">
+      <div className='flex items-center gap-3'>
+      <h1 className="text-xl font-bold">Tags</h1>
+      <button className='inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1'>
+        <Plus className='size-3'/>
+        Create New</button>
+        </div>
+     </main>
+     </div>
+  )
+}
+
