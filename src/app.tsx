4c0696ec757e5e03173864b0cdc513aa93892b7c
Wolace Luedke <wolacepin@hotmail.com>
2024-02-27 09:55:04 -0300

    feat:finalizando primeira aula

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react'
+import { Plus, Search, Filter, FileDown, MoreHorizontal } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
 import { Pagination } from './components/pagination'
-import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { useQuery, keepPreviousData } from '@tanstack/react-query'
 import { useSearchParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import useDebounceValue from './hooks/use-debounce-value'
+import { useState } from 'react'
+
 
 export interface TagResponse {
   first: number
@@ -22,43 +22,44 @@ export interface TagResponse {
 
 export interface Tag {
   title: string
-  AmountOfVideos: number
+  amountOfVideos: number
   id: string
 }
 
-
 export function App() {
-  const [searchParams, setSearchParams] = useSearchParams() 
-  const [filter, setFilter] = useState('')
+  const [searchParams, setSearchParams] = useSearchParams()
+  const urlFilter = searchParams.get('filter') ?? ''
+  
+  const [filter, setFilter] = useState(urlFilter)
 
-  const debouncedFilter = useDebounceValue(filter, 1000)
-
-    const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
-
-    useEffect(() => {
-      setSearchParams(params => {
-        params.set('page', '1')
-        return params
-      })
-    }, [debouncedFilter])
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
 
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
-    queryKey: ['get-tags',debouncedFilter, page], //salva em cache uma representação de cada página, e se adicionar page vai salvar um key por página
+    queryKey: ['get-tags', urlFilter, page],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`)
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
       const data = await response.json()
 
-      await new Promise(resolve => setTimeout(resolve, 2000)) 
+      // delay 2s
+      await new Promise(resolve => setTimeout(resolve, 2000))
 
       return data
     },
-    placeholderData: keepPreviousData, //usa para não piscar quando troca de página
-    staleTime: 1000 * 60
+    placeholderData: keepPreviousData,
   })
 
-if(isLoading) {
-  return null
-}
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  if (isLoading) {
+    return null
+  }
 
   return (
     <div className="py-10 space-y-8">
@@ -76,12 +77,20 @@ if(isLoading) {
         </div>
 
         <div className="flex items-center justify-between">
-          <Input variant='filter'>
-            <Search className="size-3" />
-            <Control placeholder="Search tags..." 
-           onChange={e => setFilter(e.target.value)}
-           value={filter} />
-          </Input>
+          <div className="flex items-center">
+            <Input variant='filter'>
+              <Search className="size-3" />
+              <Control 
+                placeholder="Search tags..." 
+                onChange={e => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+            <Button onClick={handleFilter}>
+              <Filter className="size-3" />
+              Filter
+            </Button>
+          </div>
 
           <Button>
             <FileDown className="size-3" />
@@ -110,7 +119,7 @@ if(isLoading) {
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
-                    {tag.AmountOfVideos}
+                    {tag.amountOfVideos} video(s)
                   </TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
@@ -122,7 +131,8 @@ if(isLoading) {
             })}
           </TableBody>
         </Table>
-            {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
+
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
       </main>
     </div>
   )
